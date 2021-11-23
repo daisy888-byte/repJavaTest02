755c7fa252068d22708089664593968e464b58c3
apple <561373484@qq.com>
2021-11-23 15:42:56 +0800

    add two class :Teacher,Grade.

---
+++ src/main/java/com/bjpn/domain/Teacher.java
@@ -0,0 +1,31 @@
+package com.bjpn.domain;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private String professional;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfessional() {
+        return professional;
+    }
+
+    public void setProfessional(String professional) {
+        this.professional = professional;
+    }
+}
