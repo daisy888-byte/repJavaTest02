134ba750329afbe3aaf972fb2b0eb13f4c511dac
apple <561373484@qq.com>
2021-11-23 15:45:36 +0800

    add school class.

---
+++ src/main/java/com/bjpn/domain/School.java
@@ -0,0 +1,22 @@
+package com.bjpn.domain;
+
+public class School {
+    private String schName;
+    private String schAddr;
+
+    public String getSchName() {
+        return schName;
+    }
+
+    public void setSchName(String schName) {
+        this.schName = schName;
+    }
+
+    public String getSchAddr() {
+        return schAddr;
+    }
+
+    public void setSchAddr(String schAddr) {
+        this.schAddr = schAddr;
+    }
+}
